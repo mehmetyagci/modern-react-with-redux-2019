689155c5c5994013a5fb06b19353dc2b005a9456
mehmet yagci <mehmetyagci53@hotmail.com>
2019-11-27 22:21:35 +0300

    34. Passing and Receiving Props
    34. Passing and Receiving Props

--- 03_components/src/index.js
+++ 03_components/src/index.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+
+      <CommentDetail author={faker.name.firstName ()} />
+      <CommentDetail author={faker.name.firstName ()} />
+      <CommentDetail author={faker.name.firstName ()} />
+
     </div>
   );
 };
