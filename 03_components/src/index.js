c4ea54bdde5969113a2bb64117dbeb469c663991
mehmet yagci <mehmetyagci53@hotmail.com>
2019-11-28 19:09:01 +0300

    40. Component Reuse

--- 03_components/src/index.js
+++ 03_components/src/index.js
@@ -1,30 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
 
-      <CommentDetail
-        author={faker.name.firstName ()}
-        timeAgo="Today at 4:45PM"
-        content={faker.random.words (5)}
-        avatar={faker.image.avatar ()}
-      />
-      <CommentDetail
-        author={faker.name.firstName ()}
-        timeAgo="Today at 2:15AM"
-        content={faker.random.words (5)}
-        avatar={faker.image.avatar ()}
-      />
-      <CommentDetail
-        author={faker.name.firstName ()}
-        timeAgo="Today at 5:00PM"
-        content={faker.random.words (5)}
-        avatar={faker.image.avatar ()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName ()}
+          timeAgo="Today at 4:45PM"
+          content={faker.random.words (5)}
+          avatar={faker.image.avatar ()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName ()}
+          timeAgo="Today at 2:15AM"
+          content={faker.random.words (5)}
+          avatar={faker.image.avatar ()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName ()}
+          timeAgo="Today at 5:00PM"
+          content={faker.random.words (5)}
+          avatar={faker.image.avatar ()}
+        />
+      </ApprovalCard>
 
     </div>
   );
