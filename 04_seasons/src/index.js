ce792958a70e99d5cf93d1a85c3a81b15f1e3906
mehmet yagci <mehmetyagci53@hotmail.com>
2019-11-30 21:23:43 +0300

    54. Updating State Properties

--- 04_seasons/src/index.js
+++ 04_seasons/src/index.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
-
 class App extends React.Component {
   constructor (props) {
     super (props);
-    this.state = {
-      lat: null,
-    };
-  }
 
-  render () {
+    // THIS IS THE ONLY TIME we do direct assignment
+    this.state = {lat: null};
+
     window.navigator.geolocation.getCurrentPosition (
-      position => console.log (position),
+      position => {
+        console.log (position);
+        this.setState ({lat: position.coords.latitude}); // we called setstate!!!!
+      },
       err => console.log (err)
     );
+  }
 
-    return (
-      <div>
-        <h1>Latitude:</h1>
-        <SeasonDisplay />
-      </div>
-    );
+  //React says we have to define render!!
+  render () {
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
