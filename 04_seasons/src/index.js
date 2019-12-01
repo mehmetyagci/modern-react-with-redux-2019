337044ce123cdabdd2f2a3edecc65c13229a6774
mehmet yagci <mehmetyagci53@hotmail.com>
2019-12-01 21:28:14 +0300

    56. Handling Errors Gracefully

--- 04_seasons/src/index.js
+++ 04_seasons/src/index.js
@@ -5,20 +5,33 @@ class App extends React.Component {
     super (props);
 
     // THIS IS THE ONLY TIME we do direct assignment
-    this.state = {lat: null};
+    this.state = {lat: null, errorMessage: ''};
 
     window.navigator.geolocation.getCurrentPosition (
       position => {
         console.log (position);
         this.setState ({lat: position.coords.latitude}); // we called setstate!!!!
       },
-      err => console.log (err)
+      // err => console.log (err)
+      err => {
+        console.log (err);
+        this.setState ({errorMessage: err.message});
+      }
     );
   }
 
   //React says we have to define render!!
   render () {
-    return <div>Latitude: {this.state.lat}</div>;
+    const {lat, errorMessage} = this.state;
+    if (errorMessage.length > 0) {
+      return <div>Error:{errorMessage}</div>;
+    }
+
+    return (
+      <div>
+        Latitude: {lat}
+      </div>
+    );
   }
 }
 
