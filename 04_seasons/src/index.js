8d8984ce92776996cf1e4680665f14c1ee80bbbc
mehmet yagci <mehmetyagci53@hotmail.com>
2019-12-02 20:37:50 +0300

    61. Alternate State Initialization

--- 04_seasons/src/index.js
+++ 04_seasons/src/index.js
@@ -1,27 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 class App extends React.Component {
-  constructor (props) {
-    super (props);
+  // constructor (props) {
+  //   super (props);
 
-    // THIS IS THE ONLY TIME we do direct assignment
-    this.state = {lat: null, errorMessage: ''};
+  //   // THIS IS THE ONLY TIME we do direct assignment
+  //   this.state = {lat: null, errorMessage: ''};
+  // }
+
+  state = {lat: null, errorMessage: ''};
+
+  componentDidMount () {
+    console.log (
+      'componentDidMount -> My component was rendered to the screen'
+    );
+
+    // window.navigator.geolocation.getCurrentPosition (
+    //   position => {
+    //     console.log (position);
+    //     this.setState ({lat: position.coords.latitude}); // we called setstate!!!!
+    //   },
+    //   // err => console.log (err)
+    //   err => {
+    //     console.log (err);
+    //     this.setState ({errorMessage: err.message});
+    //   }
+    // );
 
     window.navigator.geolocation.getCurrentPosition (
-      position => {
-        console.log (position);
-        this.setState ({lat: position.coords.latitude}); // we called setstate!!!!
-      },
-      // err => console.log (err)
-      err => {
-        console.log (err);
-        this.setState ({errorMessage: err.message});
-      }
+      position => this.setState ({lat: position.coords.latitude}),
+      err => this.setState ({errorMessage: err.message})
     );
+  }
+
+  componentDidUpdate () {
+    console.log (
+      'componentDidUpdate -> My component was just updated - it rendered!'
+    );
+  }
+
+  componentWillUnmount () {
+    console.log ('componentWillUnmount');
   }
 
   //React says we have to define render!!
   render () {
+    console.log ('render -> My component was just rendered!');
+
     const {lat, errorMessage} = this.state;
 
     if (errorMessage && !lat) {
