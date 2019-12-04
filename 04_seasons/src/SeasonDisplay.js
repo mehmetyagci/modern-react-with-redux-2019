00dd535f314212a0b5232f3f6b32d3fb2cd3a536
mehmet yagci <mehmetyagci53@hotmail.com>
2019-12-04 19:29:23 +0300

    65. Showing Icons
    65. Showing Icons

--- 04_seasons/src/SeasonDisplay.js
+++ 04_seasons/src/SeasonDisplay.js
@@ -11,17 +11,18 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = props => {
   console.log ('SeasonDisplay->props');
   const season = getSeason (props.lat, new Date ().getMonth ());
-  const text = season === 'winter'
-    ? 'burr, it is chilly'
-    : 'Lets hit the beach';
+  const text = season === 'winter' ? 'burr, it is chilly' : 'Lets hit the beach';
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
 
   console.log (props);
   return (
     <div>
-      {/*   {season === 'winter' ? 'burr, it is chilly' : 'Lets hit the beach'} */}
+      {/*   {season === 'winter' ? 'burr, it is chilly' : 'Lets hit the beach'} 
       <br />
-      <hr />
+      <hr />*/}
+      <i className={`${icon} icon`} />
       <h1>{text}</h1>
+      <i className={`${icon} icon`} />
     </div>
   );
 };
