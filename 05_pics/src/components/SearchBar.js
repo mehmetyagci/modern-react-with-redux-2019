391afdd584c5bf5ada118421e2e069be0def2071
mehmet yagci <mehmetyagci53@hotmail.com>
2019-12-06 23:35:52 +0300

    79. Creating Event Handlers
    79. Creating Event Handlers

--- 05_pics/src/components/SearchBar.js
+++ 05_pics/src/components/SearchBar.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class SearchBar extends Component {
-    render() {
-        return (
-            <div>
-               <form>
-                <input type="text" />
-               </form>
-            </div>
-        );
-    }
+  onInputChange (event) {
+    console.log (event.target.value);
+  }
+
+  render () {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input type="text" onChange={this.onInputChange} />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
