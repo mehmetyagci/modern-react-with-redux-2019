b29cf715a713f87dfb91645b3e99eb3dcb997a03
mehmet yagci <mehmetyagci53@hotmail.com>
2019-12-06 23:09:34 +0300

    05_pics projects created.
    05_pics projects created.

---
+++ 05_pics/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+      App
+    </div>
+  );
+};
+
+export default App;
