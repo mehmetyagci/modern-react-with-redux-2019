a19816e86a1b530e946cb06e4e2a6aac8879f958
mehmet yagci <mehmetyagci53@hotmail.com>
2019-12-07 23:00:31 +0300

    83. Handling Form Submittal
    83. Handling Form Submittal

--- 05_pics/src/components/SearchBar.js
+++ 05_pics/src/components/SearchBar.js
@@ -3,10 +3,16 @@ import React, {Component} from 'react';
 class SearchBar extends Component {
   state = {term: 'BMW'};
 
+  onFormSubmit (event) {
+    event.preventDefault (); // cancelation of form submit automatically
+
+    console.log(this.state.term)
+  }
+
   render () {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
             <input
