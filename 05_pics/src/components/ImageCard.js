b815258b5499e7f45210f6afc7198d89507f2d9b
mehmet yagci <mehmetyagci53@hotmail.com>
2019-12-12 21:55:24 +0300

    Section 10 Completed.
    Section 10 Completed.

--- 05_pics/src/components/ImageCard.js
+++ 05_pics/src/components/ImageCard.js
@@ -1,12 +1,39 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor (props) {
+    super (props);
+
+    this.state = {spans: 0};
+
+    this.imageRef = React.createRef ();
+  }
+
+  componentDidMount () {
+    console.log (this.imageRef);
+    console.log (this.imageRef.current.clientHeight);
+    // that moment we don't know the current image Height
+    // we have to add Event Listener to that img
+    this.imageRef.current.addEventListener ('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    console.log ('setSpans');
+    console.log (this.imageRef.current.clientHeight);
+
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil (height / 10);
+
+    this.setState ({spans});
+  };
+
   render () {
     const {description, urls} = this.props.image;
 
     return (
-      <div>
-        <img alt={description} src={urls.regular} />
+      <div style={{gridRowEnd: `span ${this.state.spans}`}}>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
 
       </div>
     );
