ccbf56ebdb58fc2dcbc8ba8b35a2583546c7f8e9
mehmet yagci <mehmetyagci53@hotmail.com>
2019-12-14 15:26:43 +0300

    asd
    asd

--- 5_videos/src/apis/youtube.js
+++ 5_videos/src/apis/youtube.js
@@ -1,3 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyBH-HqAC5irr7WcKUwnaHKx5tbvg6RKngE';
 
-
+export default axios.create ({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY,
+  },
+});
