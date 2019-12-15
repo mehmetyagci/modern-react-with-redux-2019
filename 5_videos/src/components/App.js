8f531a8f6dd2a9c19576e549945d706f00027e8b
mehmet yagci <mehmetyagci53@hotmail.com>
2019-12-15 11:03:20 +0300

    5 videos completed

--- 5_videos/src/components/App.js
+++ 5_videos/src/components/App.js
@@ -2,10 +2,16 @@ import React, {Component} from 'react';
 
 import youtubeAPI from '../apis/youtube';
 
-import VideoList from './VideoList';
 import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
+
 class App extends Component {
   state = {videos: [], selectedVideo: null};
+
+  componentDidMount () {
+    this.onTermSubmit ('buildings');
+  }
 
   onTermSubmit = async term => {
     console.log ('App->onTermSubmit->term:', term);
@@ -16,18 +22,35 @@ class App extends Component {
     });
     console.log ('response');
     console.log (response);
-    this.setState ({videos: response.data.items});
+    this.setState ({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
   };
 
   onVideoSelect = video => {
     console.log ('From the App!', video);
+    this.setState ({selectedVideo: video});
   };
 
   render () {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- 5_videos/src/components/VideoItem.js
+++ 5_videos/src/components/VideoItem.js
@@ -5,11 +5,7 @@ import React from 'react';
 const VideoItem = ({video, onVideoSelect}) => {
   return (
     <div className="item video-item"   onClick={() => onVideoSelect(video)} >
-      <img
-        className="ui image"
-        alt={video.snippet.title}
-        src={video.snippet.thumbnails.medium.url}
-      />
+      <img className="ui image" alt={video.snippet.title} src={video.snippet.thumbnails.medium.url}/>
       <div className="content">
         <div className="header">
           {video.snippet.title}
