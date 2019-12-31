eb7ef64e4efc73f846e9a7036c4dde737cc2b79b
mehmet yagci <mehmetyagci53@hotmail.com>
2019-12-31 22:05:50 +0300

    asd
    asd

--- 7_blog/src/components/UserHeader.js
+++ 7_blog/src/components/UserHeader.js
@@ -9,8 +9,8 @@ class UserHeader extends React.Component {
   }
 
   render () {
-    const user = this.props.users.find (user => user.id === this.props.userId);
-
+    const {user} = this.props;
+    
     if (!user) {
       return null;
     }
@@ -19,8 +19,8 @@ class UserHeader extends React.Component {
   }
 }
 
-const mapStateToProps = state => {
-  return {users: state.users};
+const mapStateToProps = (state, ownProps) => {
+  return {user: state.users.find (user => user.id === ownProps.userId)};
 };
 
 export default connect (mapStateToProps, {fetchUser}) (UserHeader);
