f2a558447de3217e754907834ca2554c4340c18e
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-01 21:32:37 +0300

    193. App Wrapup  - Section 15 completed
    193. App Wrapup  - Section 15 completed

--- 7_blog/src/actions/index.js
+++ 7_blog/src/actions/index.js
@@ -4,19 +4,11 @@ import jsonPlaceHolder from '../apis/jsonPlaceHolder';
 
 // Action Creatros in Action Creators!
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
-  console.log ('About to fetch posts');
+  console.log ('About to fetch posts and users');
   await dispatch (fetchPosts ());
   console.log (getState ().posts);
   console.log ('fetched posts!');
 
-  // // getting userIds first method
-  // const userIds = _.uniq (_.map (getState ().posts, 'userId'));
-  // console.log ('userIds:', userIds);
-  // userIds.forEach (id => dispatch (fetchUser (id)));
-
-  // await Promise.all (userIds.map (id => dispatch (fetchUser (id))));
-
-  // getting userIds second method
   _.chain (getState ().posts)
     .map ('userId')
     .uniq ()
@@ -35,58 +27,3 @@ export const fetchUser = id => async dispatch => {
 
   dispatch ({type: 'FETCH_USER', payload: response.data});
 };
-
-//#region memoize
-
-// export const fetchUser = id => dispatch => {
-//   _fetchUser (id, dispatch);
-// };
-
-// // private method for memoize
-// const _fetchUser = _.memoize (async (id, dispatch) => {
-//   const response = await jsonPlaceHolder.get (`/users/${id}`);
-
-//   dispatch ({type: 'FETCH_USER', payload: response.data});
-// });
-
-//#endregion memoize
-
-// export const fetchUser = (id) => async dispatch => {
-//   const response = await jsonPlaceHolder.get (`/users/${id}`);
-
-//   dispatch ({type: 'FETCH_USER', payload: response.data});
-// };
-
-// export const fetchUser = function (id) {
-//   return _.memoize (async function (dispatch) {
-//     const response = await jsonPlaceHolder.get (`/users/${id}`);
-
-//     dispatch ({type: 'FETCH_USER', payload: response.data});
-//   });
-// };
-
-// export const fetchPosts = () => {
-//   return async function (dispatch, getState) {
-//     const response = await jsonPlaceHolder.get ('/posts');
-
-//     dispatch ({type: 'FETCH_POSTS', payload: response});
-//   };
-// };
-
-// export const fetchPosts = () => {
-//   return function (dispatch, getState) {
-//     const promise = jsonPlaceHolder.get ('/posts');
-
-//     return {
-//       type: 'FETCH_POSTS',
-//       payload: promise,
-//     };
-//   };
-// };
-
-// // TOTALY FINE!
-// export const selectPost = () => {
-//   return {
-//     type: 'SELECT_POST',
-//   };
-// };
