bcbd73abb2b2629b6baa82151671fa2aefab5f52
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-08 07:35:32 +0300

    208. Links Inside Routers

--- 8_streams/client/src/components/App.js
+++ 8_streams/client/src/components/App.js
@@ -1,31 +1,25 @@
 import React from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 
-const PageOne = () => {
-  return (
-    <div>
-      PageOne
-      <Link to="/pagetwo">Navigate to Page Two</Link>
-    </div>
-  );
-};
+import Header from './Header';
 
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <button>Click Me!</button>
-      <Link to="/">Navigate to Page One</Link>
-    </div>
-  );
-};
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
-        <Route path="/" exact component={PageOne} />
-        <Route path="/pagetwo" exact component={PageTwo} />
+        <Header />
+
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" exact component={StreamCreate} />
+        <Route path="/streams/edit" exact component={StreamEdit} />
+        <Route path="/streams/delete" exact component={StreamDelete} />
+        <Route path="/streams/show" exact component={StreamShow} />
       </BrowserRouter>
     </div>
   );
