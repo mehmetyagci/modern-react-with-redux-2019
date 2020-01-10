88c18fe7868c34ab823ba93e66687f63696622ff
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-10 11:19:30 +0300

    221. Building the Auth Reducer

---
+++ 8_streams/client/src/actions/index.js
@@ -0,0 +1,13 @@
+export const signIn = () => {
+  return {
+    type: 'SIGN_IN',
+  };
+};
+
+export const signOut = () => {
+  return {
+    type: 'SIGN_OUT',
+  };
+};
+
+
--- 8_streams/client/src/components/GoogleAuth.js
+++ 8_streams/client/src/components/GoogleAuth.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
+import {signIn, signOut} from '../actions';
 
 class GoogleAuth extends React.Component {
   state = {isSignedIn: null};
@@ -18,8 +21,12 @@ class GoogleAuth extends React.Component {
     });
   }
 
-  onAuthChange = () => {
-    this.setState ({isSignedIn: this.auth.isSignedIn.get ()});
+  onAuthChange = isSignedIn => {
+    if (isSignedIn) {
+      this.props.signIn ();
+    } else {
+      this.props.signOut ();
+    }
   };
 
   onSignInClick = () => {
@@ -55,4 +62,4 @@ class GoogleAuth extends React.Component {
   }
 }
 
-export default GoogleAuth;
+export default connect (null, {signIn, signOut}) (GoogleAuth);
