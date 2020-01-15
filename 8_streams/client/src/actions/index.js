2406dc4bff0f543ad7c76eb279c16f781382b862
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-15 11:14:21 +0300

    259-260
    259. Implementing Programmatic Navigation
    260. Manually Changing API Records

--- 8_streams/client/src/actions/index.js
+++ 8_streams/client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -29,6 +30,7 @@ export const createStream = formValues => async (dispatch, getState) => {
   dispatch ({type: CREATE_STREAM, payload: response.data});
   // Do some programmatic navigation to
   // get the user back to the root route
+  history.push ('/');
 };
 
 export const fetchStreams = () => async dispatch => {
