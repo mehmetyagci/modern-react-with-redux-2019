b8f6fdb94f9aa1796b7881997a41b89a6b9e0c68
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-16 11:18:59 +0300

    264. Selecting Records from State
    264. Selecting Records from State

--- 8_streams/client/src/components/streams/StreamEdit.js
+++ 8_streams/client/src/components/streams/StreamEdit.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>;
+const StreamEdit = props => {
+  console.log ('StreamEdit:',props);
+  const id = props.match.params.id;
+  return (
+    <div>
+      StreamEdit
+      <br />
+      id: {id}
+    </div>
+  );
 };
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  console.log ('StreamEdit->ownProps:', ownProps);
+  return {stream: state.streams[ownProps.match.params.id]};
+};
+
+export default connect (mapStateToProps) (StreamEdit);
