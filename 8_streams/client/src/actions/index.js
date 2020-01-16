3dd6555a61e8a766d9442e76a657f4f3222c101e
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-16 13:56:09 +0300

    272. PUT vs PATCH Requests
    272. PUT vs PATCH Requests

--- 8_streams/client/src/actions/index.js
+++ 8_streams/client/src/actions/index.js
@@ -46,9 +46,11 @@ export const fetchStream = id => async dispatch => {
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-  const response = await streams.put (`/streams/${id}`, formValues);
+  console.log ('editStram', formValues);
+  const response = await streams.patch (`/streams/${id}`, formValues);
 
   dispatch ({type: EDIT_STREAM, payload: response.data});
+  history.push ('/');
 };
 
 export const deleteStream = id => async dispatch => {
