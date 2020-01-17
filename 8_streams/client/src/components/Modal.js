dc7669e3e8b5850f652ac59616e71c4e32039be1
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-17 12:41:07 +0300

    277. Making the Modal Reusable
    277. Making the Modal Reusable

--- 8_streams/client/src/components/Modal.js
+++ 8_streams/client/src/components/Modal.js
@@ -13,14 +13,13 @@ const Modal = props => {
         className="ui standard modal visible active"
       >
         <div className="header">
-          Delete Stream
+          {props.title}
         </div>
         <div className="content">
-          Are you sure you want to delete this stream?
+          {props.content}
         </div>
         <div className="actions">
-          <button className="ui primary button">Delete</button>
-          <button className="ui button">Cancel</button>
+          {props.actions}
         </div>
       </div>
     </div>,
