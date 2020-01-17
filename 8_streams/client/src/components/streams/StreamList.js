4dde9d6f0ad298513239e5354014696163a3c222
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-17 12:54:29 +0300

    280. Reminder on Path Params

--- 8_streams/client/src/components/streams/StreamList.js
+++ 8_streams/client/src/components/streams/StreamList.js
@@ -13,10 +13,15 @@ class StreamList extends React.Component {
     if (stream.userId === this.props.currentUserId) {
       return (
         <div className="right floated content">
-          <Link  to={`/streams/edit/${stream.id}`}  className="ui button primary">Edit</Link>
-          <button className="ui button negative">
+          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
+            Edit
+          </Link>
+          <Link
+            to={`/streams/delete/${stream.id}`}
+            className="ui button negative"
+          >
             Delete
-          </button>
+          </Link>
         </div>
       );
     }
