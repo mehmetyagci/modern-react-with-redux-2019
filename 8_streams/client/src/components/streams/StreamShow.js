45b118da7bcc4812fe1b31536eaafd9013f8d362
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-18 17:17:02 +0300

    293. Creating a FLV Player
    293. Creating a FLV Player

--- 8_streams/client/src/components/streams/StreamShow.js
+++ 8_streams/client/src/components/streams/StreamShow.js
@@ -10,7 +10,18 @@ class StreamShow extends React.Component {
     this.videoRef = React.createRef ();
   }
   componentDidMount () {
+    const {id} = this.props.match.params;
+
+    console.log ('videoRef:', this.videoRef);
+
     this.props.fetchStream (this.props.match.params.id);
+    this.player = flv.createPlayer ({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    this.player.attachMediaElement (this.videoRef.current);
+    this.player.load ();
+    this.player.play ();
   }
 
   render () {
