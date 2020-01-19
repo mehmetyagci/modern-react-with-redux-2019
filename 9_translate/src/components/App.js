088e1bdb45e2b6ca48675303268b5c821425a4c2
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-19 10:45:10 +0300

    301. A Touch More Setup
    301. A Touch More Setup

--- 9_translate/src/components/App.js
+++ 9_translate/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import UserCreate from './UserCreate';
 
 class App extends React.Component {
   state = {language: 'english'};
@@ -22,6 +23,7 @@ class App extends React.Component {
           />
         </div>
         Current Language: {this.state.language}
+        <UserCreate />
       </div>
     );
   }
