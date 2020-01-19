0e899cac56d9d1e6f1a9aa5dc56e56131da91035
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-19 10:35:20 +0300

    300. Selecting a Language
    300. Selecting a Language

---
+++ 9_translate/src/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class App extends React.Component {
+  state = {language: 'english'};
+
+  onLanguageChange = language => {
+    this.setState ({language: language});
+  };
+
+  render () {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange ('english')}
+          />
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange ('dutch')}
+          />
+        </div>
+        Current Language: {this.state.language}
+      </div>
+    );
+  }
+}
+
+export default App;
