23a108c332fa33128acf4764b63fc47f41898ab0
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-19 14:15:23 +0300

    304. Consuming the Context Value

--- 9_translate/src/components/Button.js
+++ 9_translate/src/components/Button.js
@@ -6,7 +6,8 @@ class Button extends React.Component {
 
   render () {
     console.log (this.context);
-    return <button className="ui button primary">Submit</button>;
+    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
+    return <button className="ui button primary">{text}</button>;
   }
 }
 
