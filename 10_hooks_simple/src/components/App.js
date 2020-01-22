17dccce10fcdef34650a6f3531d27af36db5e595
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-22 08:42:32 +0300

    320. UseState in Detail
    320. UseState in Detail

--- 10_hooks_simple/src/components/App.js
+++ 10_hooks_simple/src/components/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-class App extends React.Component {
-  state = {resource: 'posts'};
+const App = () => {
+  const [resource, setResource] = useState ('posts');
 
-  render () {
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <button onClick={() => this.setState ({resource: 'posts'})}>
-            Posts
-          </button>
-          <button onClick={() => this.setState ({resource: 'todos'})}>
-            Todos
-          </button>
-        </div>
-        {this.state.resource}
+        <button onClick={() => setResource ('posts')}>
+          Posts
+        </button>
+        <button onClick={() => setResource ('todos')}>
+          Todos
+        </button>
       </div>
-    );
-  }
-}
+      {resource}
+    </div>
+  );
+};
 
 export default App;
