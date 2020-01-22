cc07ff14af5d385dbeaf67779e4344f21d9cdc2b
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-22 08:33:06 +0300

    318. Building Stateful Class Components
    318. Building Stateful Class Components

---
+++ 10_hooks_simple/src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class App extends React.Component {
+  state = {resource: 'posts'};
+
+  render () {
+    return (
+      <div>
+        <div>
+          <button onClick={() => this.setState ({resource: 'posts'})}>
+            Posts
+          </button>
+          <button onClick={() => this.setState ({resource: 'todos'})}>
+            Todos
+          </button>
+        </div>
+        {this.state.resource}
+      </div>
+    );
+  }
+}
+
+export default App;
