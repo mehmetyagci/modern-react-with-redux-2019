c3cbc96e0e2c5ca0d30730df65df73fa6e9fcbfd
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-22 12:28:59 +0300

    326. Updates with UseEffect
    326. Updates with UseEffect

--- 10_hooks_simple/src/components/ResourceList.js
+++ 10_hooks_simple/src/components/ResourceList.js
@@ -1,41 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-class ResourceList extends React.Component {
-  state = {resources: []};
+const ResourceList = ({resource}) => {
+  const [resources, setResources] = useState ([]);
 
-  async componentDidMount () {
+  const fetchResource = async resource => {
     const response = await axios.get (
-      `https://jsonplaceholder.typicode.com/${this.props.resource}`
+      `https://jsonplaceholder.typicode.com/${resource}`
     );
-    this.setState ({resources: response.data});
-  }
+    setResources (response.data);
+  };
 
-  async componentDidUpdate (prevProps) {
-    console.log ('prevProps:', prevProps);
-    if (prevProps.resource !== this.props.resource) {
-      const response = await axios.get (
-        `https://jsonplaceholder.typicode.com/${this.props.resource}`
-      );
-      this.setState ({resources: response.data});
-    }
-  }
+  useEffect (
+    () => {
+      fetchResource (resource);
+    },
+    [resource]
+  );
 
-  render () {
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <h1>ResourceList</h1>
-        </div>
-        <div>
-          <h2>{this.props.resource}</h2>
-        </div>
-        <div>
-          <h3>{this.state.resources.length}</h3>
-        </div>
+        <h1>ResourceList</h1>
       </div>
-    );
-  }
-}
+      <div />
+      <div>
+        <h3>{resources.length}</h3>
+      </div>
+    </div>
+  );
+};
 
 export default ResourceList;
