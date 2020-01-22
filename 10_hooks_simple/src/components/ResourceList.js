2f2675605c2a280927b380a0b0b9d4f9cfe63b49
mehmet yagci <mehmetyagci53@hotmail.com>
2020-01-22 11:30:17 +0300

    323. ComponentDidUpdate vs ComponentDidMount
    323. ComponentDidUpdate vs ComponentDidMount

--- 10_hooks_simple/src/components/ResourceList.js
+++ 10_hooks_simple/src/components/ResourceList.js
@@ -11,6 +11,16 @@ class ResourceList extends React.Component {
     this.setState ({resources: response.data});
   }
 
+  async componentDidUpdate (prevProps) {
+    console.log ('prevProps:', prevProps);
+    if (prevProps.resource !== this.props.resource) {
+      const response = await axios.get (
+        `https://jsonplaceholder.typicode.com/${this.props.resource}`
+      );
+      this.setState ({resources: response.data});
+    }
+  }
+
   render () {
     return (
       <div>
